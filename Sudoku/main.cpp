30a284db5172af468e7bd73fb599b08c5b2af8d2
“wiesnejt” <wiesnejt@mail.uc.edu>
2017-03-22 18:37:32 -0400

    overlap testing still

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -6,16 +6,11 @@ using namespace std;
 
 int main()
 {
-    game.run();
-    // How are you?
-    cout << "testing" << endl;
- //   cout << "I'm on a boat" << endl;
- //   cout << "      O      " << endl;
-  //  cout << "     /|\\    " << endl;
-//    cout << "     / \\    " << endl;
-//    cout << "  <_______>  " << endl;
     game game1;
     game1.run();
+    // How are you?
+    cout << "testing" << endl;
+
     cout << "I don't knwo what this will do";
     cout << "hi\n" << endl;
     cout << "I'm on a boat" << endl;
