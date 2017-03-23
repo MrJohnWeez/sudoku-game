89bf07e96734c73e3d87a31652110e5d2ffec549
“wiesnejt” <wiesnejt@mail.uc.edu>
2017-03-23 11:06:02 -0400

    Fixed setting board values
    Difficulty works now, boards print out, used by reference '&' to pass in/change boards

--- Sudoku/sudoku_class.cpp
+++ Sudoku/sudoku_class.cpp
@@ -16,14 +16,17 @@ using namespace std;
 
 
 
-
-
+struct holdBoard{
+    int boardPlay[9][9] = {{0}};
+    int boardKey[9][9] = {{0}};
+};
 
 struct gameboard{
     string title;
-    int board[9][9] = {{0}};
-    int boardkey[9][9] = {{0}};
+    holdBoard board;
 };
+
+
 
 class game{
 public:
@@ -44,15 +47,87 @@ private:
         cin.get();
     }
 
-    void assign(gameboard playBoard, int startBoard[][9], int keyBoard[][9]){
+    void updateBoard(gameboard &start, int main[][9], int key[][9]){
         for(unsigned int row = 0; row < 9; row++){
             for(unsigned int col = 0; col < 9; col++){
-                playBoard.board[row][col] = startBoard[row][col];
-                playBoard.boardkey[row][col] = keyBoard[row][col];
+                start.board.boardPlay[row][col] = main[row][col];
+                start.board.boardKey[row][col] = key[row][col];
             }
         }
-        cout << "Went through" << endl;
     }
+
+    gameboard chooseBoard(int option, gameboard &start){
+        //easy
+        if(option == 1){
+            int main[9][9] = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
+                              {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                              {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                              {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                              {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                              {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                              {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                              {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                              {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+            int key[9][9] = {{3, 1, 6, 5, 7, 8, 4, 9, 2},
+                             {5, 2, 9, 1, 3, 4, 7, 6, 8},
+                             {4, 8, 7, 6, 2, 9, 5, 3, 1},
+                             {2, 6, 3, 4, 1, 5, 9, 8, 7},
+                             {9, 7, 4, 8, 6, 3, 1, 2, 5},
+                             {8, 5, 1, 7, 9, 2, 6, 4, 3},
+                             {1, 3, 8, 9, 4, 7, 2, 5, 6},
+                             {6, 9, 2, 3, 5, 1, 8, 7, 4},
+                             {7, 4, 5, 2, 8, 6, 3, 1, 9}};
+            //changes boards to selected option
+            updateBoard(start, main, key);
+
+            //medium
+        }else if(option == 2){
+            int main[9][9] = {{4, 0, 6, 5, 0, 8, 4, 0, 0},
+                              {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                              {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                              {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                              {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                              {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                              {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                              {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                              {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+            int key[9][9] = {{3, 1, 6, 5, 7, 8, 4, 9, 2},
+                             {5, 2, 9, 1, 3, 4, 7, 6, 8},
+                             {4, 8, 7, 6, 2, 9, 5, 3, 1},
+                             {2, 6, 3, 4, 1, 5, 9, 8, 7},
+                             {9, 7, 4, 8, 6, 3, 1, 2, 5},
+                             {8, 5, 1, 7, 9, 2, 6, 4, 3},
+                             {1, 3, 8, 9, 4, 7, 2, 5, 6},
+                             {6, 9, 2, 3, 5, 1, 8, 7, 4},
+                             {7, 4, 5, 2, 8, 6, 3, 1, 9}};
+            updateBoard(start, main, key);
+
+        //hard
+        }else{
+            int main[9][9] = {{5, 0, 6, 5, 0, 8, 4, 0, 0},
+                                    {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                                    {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                                    {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                                    {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                                    {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                                    {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                                    {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                                    {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+            int key[9][9] = {{3, 1, 6, 5, 7, 8, 4, 9, 2},
+                                       {5, 2, 9, 1, 3, 4, 7, 6, 8},
+                                       {4, 8, 7, 6, 2, 9, 5, 3, 1},
+                                       {2, 6, 3, 4, 1, 5, 9, 8, 7},
+                                       {9, 7, 4, 8, 6, 3, 1, 2, 5},
+                                       {8, 5, 1, 7, 9, 2, 6, 4, 3},
+                                       {1, 3, 8, 9, 4, 7, 2, 5, 6},
+                                       {6, 9, 2, 3, 5, 1, 8, 7, 4},
+                                       {7, 4, 5, 2, 8, 6, 3, 1, 9}};
+            updateBoard(start, main, key);
+        }
+
+        return start;
+    }
+
 
     gameboard boardSelect(){
         int diff = -1;
@@ -69,40 +144,20 @@ private:
 
         gameboard playBoard;
         playBoard.title = "Easy #1";
-        int easyBoard1[9][9] = {{3, 0, 6, 5, 0, 8, 4, 0, 0},
-                                {5, 2, 0, 0, 0, 0, 0, 0, 0},
-                                {0, 8, 7, 0, 0, 0, 0, 3, 1},
-                                {0, 0, 3, 0, 1, 0, 0, 8, 0},
-                                {9, 0, 0, 8, 6, 3, 0, 0, 5},
-                                {0, 5, 0, 0, 9, 0, 6, 0, 0},
-                                {1, 3, 0, 0, 0, 0, 2, 5, 0},
-                                {0, 0, 0, 0, 0, 0, 0, 7, 4},
-                                {0, 0, 5, 2, 0, 6, 3, 0, 0}};
-        int easyBoard1KEY[9][9] = {{3, 1, 6, 5, 7, 8, 4, 9, 2},
-                                   {5, 2, 9, 1, 3, 4, 7, 6, 8},
-                                   {4, 8, 7, 6, 2, 9, 5, 3, 1},
-                                   {2, 6, 3, 4, 1, 5, 9, 8, 7},
-                                   {9, 7, 4, 8, 6, 3, 1, 2, 5},
-                                   {8, 5, 1, 7, 9, 2, 6, 4, 3},
-                                   {1, 3, 8, 9, 4, 7, 2, 5, 6},
-                                   {6, 9, 2, 3, 5, 1, 8, 7, 4},
-                                   {7, 4, 5, 2, 8, 6, 3, 1, 9}};
-
-
-        assign(playBoard, easyBoard1, easyBoard1KEY);
+        chooseBoard(diff, playBoard);
 
         //Temp Printing
         int row, col = 0;
         for(row = 0; row < 9; row++){
             for(col = 0; col < 9; col++){
-                cout << playBoard.board[row][col];
+                cout << playBoard.board.boardPlay[row][col];
             }
             cout << endl;
         }
-        cout << "\n\n\n" << endl;
+        cout << "\n" << endl;
         for(row = 0; row < 9; row++){
             for(col = 0; col < 9; col++){
-                cout << playBoard.boardkey[row][col];
+                cout << playBoard.board.boardKey[row][col];
             }
             cout << endl;
         }
